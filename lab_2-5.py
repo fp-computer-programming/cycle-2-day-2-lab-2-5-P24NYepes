f7fe446a8972b40ed78410026985ee0bf30ad391
Nicholas Yepes <p24nyepes@fairfieldprep.org>
2023-09-26 10:45:11 -0400

    Submit Lab

--- lab_2-5.py
+++ lab_2-5.py
@@ -15,5 +15,25 @@ or not using augmented assignments. To start, set all variables used equal to 21
  a *= 12
  i = i // 7
 """
-
+x=21
+y=21
+z=21
+w=21
+t=21
+a=21
+i=21
 # Be sure to include comments above each pair of 2 lines with the question number (Ex. # Question 1) and print statements beneath each assignment.
+#question 1
+x=x+5
+#question 2
+y-=3
+#question 3
+z=z**2
+#question 4
+w%=2
+#question 5S
+t/=5
+#question 6
+a=a*12
+#question 7
+i//=7
